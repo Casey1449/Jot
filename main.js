538827f58d04fe15ce5f8b11a00a2885c6fd530d
Casey Metz <caseymetz@gmail.com>
2016-12-06 15:40:56 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+
+  mainWindow = new BrowserWindow();
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+});
