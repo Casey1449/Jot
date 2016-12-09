71882d5cb4a423682a9c932449c293cd0609949a
Casey Metz <caseymetz@gmail.com>
2016-12-09 14:06:08 -0700

    commit-to-test

--- tests/test.js
+++ tests/test.js
@@ -14,7 +14,7 @@ global.before(function () {
     chai.use(chaiAsPromised);
 });
 
-describe('App starts and has correct title and buttons', function () {
+describe('App starts and has correct initial appearance', function () {
   var app = null
   beforeEach(function () {
       app = new Application({ path: electronPath, args: [appPath]});
@@ -28,5 +28,28 @@ describe('App starts and has correct title and buttons', function () {
   it('opens a window', function () {
     return app.client.waitUntilWindowLoaded()
       .getWindowCount().should.eventually.equal(1);
+  });
+
+  it('tests the title', function () {
+  return app.client.waitUntilWindowLoaded()
+    .getTitle().should.eventually.equal('Jot');
+  });
+
+  it('displays an "add notebook" button', function (){
+    return app.client.getText('#add-notebook-button').then(function (buttonText) {
+    assert(buttonText === 'Create Notebook')
+    })
+  });
+
+  xit('displays a "save note" button', function (){
+    return app.client.getText('#save-markdown').then(function (buttonText) {
+    assert(buttonText === 'Save File')
+    })
+  });
+
+  xit('displays a "delete note" button', function (){
+    return app.client.getText('#save-markdown').then(function (buttonText) {
+    assert(buttonText === 'Save File')
+    })
   });
 });
