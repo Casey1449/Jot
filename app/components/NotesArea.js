d47305d47e3fc269c6684d4e61ead87681a6bdc7
Madison Kerndt <madison.kerndt@gmail.com>
2016-12-10 12:44:15 -0700

    add test descriptions

--- app/components/NotesArea.js
+++ app/components/NotesArea.js
@@ -6,12 +6,12 @@ export default (props) => {
     <section className='notes-area'>
 
       <textarea
-        className='note-text'
+        className='note-input-text'
         value={ props.content }
         onChange={ (e) => props.setNote(e) }
         placeholder='add your note'
         />
-      <button onClick={ props.saveNote }>Save note</button>
+      <button className='note-save-button' onClick={ props.saveNote }>Save note</button>
       <button onClick={ props.destroyNote }>Delete note</button>
       <button onClick={ props.startNewNote }>Add new note</button>
 
