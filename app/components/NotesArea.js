84e33d3adef20788b355b10bb290ef9ba9d174a3
Madison Kerndt <madison.kerndt@gmail.com>
2016-12-10 13:02:04 -0700

    save on focus out

--- app/components/NotesArea.js
+++ app/components/NotesArea.js
@@ -6,12 +6,12 @@ export default (props) => {
     <section className='notes-area'>
 
       <textarea
+        onBlur={ props.saveNote }
         className='note-text'
         value={ props.content }
         onChange={ (e) => props.setNote(e) }
         placeholder='add your note'
         />
-      <button onClick={ props.saveNote }>Save note</button>
       <button onClick={ props.destroyNote }>Delete note</button>
       <button onClick={ props.startNewNote }>Add new note</button>
 
