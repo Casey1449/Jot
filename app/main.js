d6f5650685ed383fae1994d3391a8d23eddca274
Madison Kerndt <madison.kerndt@gmail.com>
2016-12-11 13:21:49 -0700

    add dev tools

--- app/main.js
+++ app/main.js
@@ -1,9 +1,9 @@
 const { app, BrowserWindow } = require('electron');
-// import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
-//
-// installExtension(REACT_DEVELOPER_TOOLS)
-//     .then((name) => console.log(`Added Extension:  ${name}`))
-//     .catch((err) => console.log('An error occurred: ', err));
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+
+installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
 
 let mainWindow = null;
 
