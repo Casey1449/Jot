08feb28cb6450dff7c8bdeae2c606b6ac0bcd2f7
Casey Metz <caseymetz@gmail.com>
2016-12-11 19:18:25 -0700

    add tray and dock icons!

--- app/main.js
+++ app/main.js
@@ -1,19 +1,31 @@
-const { app, BrowserWindow } = require('electron');
+const { nativeImage, clipboard, app, Menu, Tray, BrowserWindow } = require('electron');
 
-// import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
-//
-// installExtension(REACT_DEVELOPER_TOOLS)
-//    .then((name) => console.log(`Added Extension:  ${name}`))
-//    .catch((err) => console.log('An error occurred: ', err));
 
 let mainWindow = null;
+let tray = null;
 
 app.on('ready', function() {
- mainWindow = new BrowserWindow({
- });
 
- mainWindow.loadURL(`file://${__dirname}/index.html`)
- mainWindow.on('closed', function() {
+  let trayImage = nativeImage.createFromPath('/Users/caseymetz/Turing/Mod4-projects/jot/jot_logo_png.png').resize({width: 16, height: 16});
+
+  let dockImage =  nativeImage.createFromPath('/Users/caseymetz/Turing/Mod4-projects/jot/jot_logo_png.png').resize({width: 300, height: 300});
+
+  app.dock.setIcon(dockImage);
+
+  tray = new Tray(trayImage);
+
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Item1', type: 'radio'},
+    {label: 'Item2', type: 'radio'},
+  ])
+
+  tray.setToolTip('This is my application');
+  tray.setContextMenu(contextMenu)
+  mainWindow = new BrowserWindow({
+   });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.on('closed', function() {
    mainWindow = null;
- });
+  });
 });
